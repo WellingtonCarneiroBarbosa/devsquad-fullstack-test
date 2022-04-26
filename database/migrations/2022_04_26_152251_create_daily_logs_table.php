97c9d20c4d78c9261c20fdff2cf586f80099ea45
Wellington Barbosa <carneirobarbosawellington@gmail.com>
2022-04-26 12:31:40 -0300

    Created daily_logs migration

---
+++ database/migrations/2022_04_26_152251_create_daily_logs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDailyLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('daily_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->index();
+            $table->text('log');
+            $table->timestamp('day');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('daily_logs');
+    }
+}
